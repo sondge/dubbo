02fecd5d1b0f5c17ed925c8fcde90654df66d32e
冯上杰 <fengshangjie@guoquan.cn>
2020-11-19 17:41:21 +0800

    新增 AbstractConfig.appendParameters 方法的注释

--- dubbo-config/dubbo-config-api/src/main/java/com/alibaba/dubbo/config/AbstractConfig.java
+++ dubbo-config/dubbo-config-api/src/main/java/com/alibaba/dubbo/config/AbstractConfig.java
@@ -42,24 +42,38 @@ import java.util.regex.Pattern;
  */
 public abstract class AbstractConfig implements Serializable {
 
+    // 定义日志系统
     protected static final Logger logger = LoggerFactory.getLogger(AbstractConfig.class);
+    // 序列化的序列号
     private static final long serialVersionUID = 4267533505537413570L;
+    // 最大长度
     private static final int MAX_LENGTH = 200;
 
+    // 最大路径长度
     private static final int MAX_PATH_LENGTH = 200;
 
+    // 名称正则
     private static final Pattern PATTERN_NAME = Pattern.compile("[\\-._0-9a-zA-Z]+");
 
+    // 多名称正则
     private static final Pattern PATTERN_MULTI_NAME = Pattern.compile("[,\\-._0-9a-zA-Z]+");
 
+    // 方法名称正则表达式
     private static final Pattern PATTERN_METHOD_NAME = Pattern.compile("[a-zA-Z][0-9a-zA-Z]*");
 
+    // 路径的正则表达式
     private static final Pattern PATTERN_PATH = Pattern.compile("[/\\-$._0-9a-zA-Z]+");
 
+    // 名称符号正则表达式校验
     private static final Pattern PATTERN_NAME_HAS_SYMBOL = Pattern.compile("[:*,\\s/\\-._0-9a-zA-Z]+");
 
+    // KEY 的正则表达式校验
     private static final Pattern PATTERN_KEY = Pattern.compile("[*,\\-._0-9a-zA-Z]+");
+
+    // 属性配置
     private static final Map<String, String> legacyProperties = new HashMap<String, String>();
+
+    // 后缀数组
     private static final String[] SUFFIXES = new String[]{"Config", "Bean"};
 
     static {
@@ -76,6 +90,7 @@ public abstract class AbstractConfig implements Serializable {
         Runtime.getRuntime().addShutdownHook(DubboShutdownHook.getDubboShutdownHook());
     }
 
+    // 配置的编号
     protected String id;
 
     private static String convertLegacyValue(String key, String value) {
@@ -90,36 +105,51 @@ public abstract class AbstractConfig implements Serializable {
     }
 
     protected static void appendProperties(AbstractConfig config) {
+        // 配置为空校验
         if (config == null) {
             return;
         }
+        // 获取配置的类名，并且增加一个前缀
         String prefix = "dubbo." + getTagName(config.getClass()) + ".";
+        // 获取方法数组
         Method[] methods = config.getClass().getMethods();
         for (Method method : methods) {
             try {
+                // 获取方法名称，
                 String name = method.getName();
+                // 判断方法名的长度，方法是否为 set 方法以及是否是公共的方法，方法的长度是否为一，方法的参数类型是否为 JAVA 支持的数据类型
                 if (name.length() > 3 && name.startsWith("set") && Modifier.isPublic(method.getModifiers())
                         && method.getParameterTypes().length == 1 && isPrimitive(method.getParameterTypes()[0])) {
+                    // 驼峰式插接名称
                     String property = StringUtils.camelToSplitName(name.substring(3, 4).toLowerCase() + name.substring(4), ".");
-
+                    // 定义变量为 null
                     String value = null;
+                    // 判断配置的编号是否为空，并且配置编号的长度是否大于 0
                     if (config.getId() != null && config.getId().length() > 0) {
+                        // 获取 名称
                         String pn = prefix + config.getId() + "." + property;
+                        // 获取配置的值（缓存机制）
                         value = System.getProperty(pn);
                         if (!StringUtils.isBlank(value)) {
                             logger.info("Use System Property " + pn + " to config dubbo");
                         }
                     }
+                    // 如果获取的配置值为空，或者配置的长度为 0
                     if (value == null || value.length() == 0) {
+                        // 使用 prefix + property 的方式作为 key
                         String pn = prefix + property;
+                        // 获取 value
                         value = System.getProperty(pn);
                         if (!StringUtils.isBlank(value)) {
                             logger.info("Use System Property " + pn + " to config dubbo");
                         }
                     }
+                    // 如果获取的 value 为空或者 value 的长度为 0
                     if (value == null || value.length() == 0) {
+                        // 获取方法
                         Method getter;
                         try {
+                            // 获取方法名称
                             getter = config.getClass().getMethod("get" + name.substring(3));
                         } catch (NoSuchMethodException e) {
                             try {
@@ -129,10 +159,13 @@ public abstract class AbstractConfig implements Serializable {
                             }
                         }
                         if (getter != null) {
+                            // 执行配置方法
                             if (getter.invoke(config) == null) {
+                                // 获取配置值，如果没有该配置，给改配置设置一个新的值
                                 if (config.getId() != null && config.getId().length() > 0) {
                                     value = ConfigUtils.getProperty(prefix + config.getId() + "." + property);
                                 }
+                                //
                                 if (value == null || value.length() == 0) {
                                     value = ConfigUtils.getProperty(prefix + property);
                                 }
@@ -168,67 +201,94 @@ public abstract class AbstractConfig implements Serializable {
         return tag;
     }
 
+    // 增加新的参数信息
     protected static void appendParameters(Map<String, String> parameters, Object config) {
         appendParameters(parameters, config, null);
     }
 
     @SuppressWarnings("unchecked")
     protected static void appendParameters(Map<String, String> parameters, Object config, String prefix) {
+        // 判断配置是否为空
         if (config == null) {
             return;
         }
+        // 获取配类的方法
         Method[] methods = config.getClass().getMethods();
         for (Method method : methods) {
             try {
                 String name = method.getName();
+                // 判断方法返回值是否为基本数据类型，并且是 get 方法，入参并且为空，也就是 get 方法
                 if ((name.startsWith("get") || name.startsWith("is"))
                         && !"getClass".equals(name)
                         && Modifier.isPublic(method.getModifiers())
                         && method.getParameterTypes().length == 0
                         && isPrimitive(method.getReturnType())) {
                     Parameter parameter = method.getAnnotation(Parameter.class);
+                    // 如果方法的返回值是 Object 类 或者注解的参数时排除，结束本次循环
                     if (method.getReturnType() == Object.class || parameter != null && parameter.excluded()) {
                         continue;
                     }
+                    // 防范名称的开头是否是 startWith 开头的，如果是，返回 3，不是返回 2
                     int i = name.startsWith("get") ? 3 : 2;
+                    // 获取驼峰式名称
                     String prop = StringUtils.camelToSplitName(name.substring(i, i + 1).toLowerCase() + name.substring(i + 1), ".");
+                    // 获取参数的 key
                     String key;
+                    // 如果配置了参数，并且参数的长度大于0
                     if (parameter != null && parameter.key().length() > 0) {
+                        // 获取参数的 key
                         key = parameter.key();
                     } else {
+                        // 设置参数的 key 是否为方法名称
                         key = prop;
                     }
+                    // 执行方法，并且获取执行结果
                     Object value = method.invoke(config);
+                    // 获取返回值的 toString
                     String str = String.valueOf(value).trim();
+                    // 如果返回值不为空， 字符串的长度大于 0
                     if (value != null && str.length() > 0) {
+                        // 如果参数不为空并且参数设置了逃脱
                         if (parameter != null && parameter.escaped()) {
+                            // 对 str 进行编码
                             str = URL.encode(str);
                         }
+                        // 如果参数不为空，并且是附加参数
                         if (parameter != null && parameter.append()) {
+                            // 获取默认的参数前缀
                             String pre = parameters.get(Constants.DEFAULT_KEY + "." + key);
+                            // 前缀为空并且前缀的长度大于 0
                             if (pre != null && pre.length() > 0) {
                                 str = pre + "," + str;
                             }
+                            // 获取参数的前缀
                             pre = parameters.get(key);
+                            //
                             if (pre != null && pre.length() > 0) {
                                 str = pre + "," + str;
                             }
                         }
+                        // 获取 key
                         if (prefix != null && prefix.length() > 0) {
                             key = prefix + "." + key;
                         }
+                        // 如果 设置参数信息
                         parameters.put(key, str);
                     } else if (parameter != null && parameter.required()) {
                         throw new IllegalStateException(config.getClass().getSimpleName() + "." + key + " == null");
                     }
+                    // 如果参数的名称为 getParameters 并且返回类型为 Map 类型
                 } else if ("getParameters".equals(name)
                         && Modifier.isPublic(method.getModifiers())
                         && method.getParameterTypes().length == 0
                         && method.getReturnType() == Map.class) {
+                    // 执行方法的参数信息
                     Map<String, String> map = (Map<String, String>) method.invoke(config, new Object[0]);
                     if (map != null && map.size() > 0) {
+                        // 获取访问前缀
                         String pre = (prefix != null && prefix.length() > 0 ? prefix + "." : "");
                         for (Map.Entry<String, String> entry : map.entrySet()) {
+                            // 将参数信息直接 put 到 parameters 这个参数信息
                             parameters.put(pre + entry.getKey().replace('-', '.'), entry.getValue());
                         }
                     }
